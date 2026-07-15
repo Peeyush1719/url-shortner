abe5b157576467c0cf83fa54a53a02407fa305d3
Peeyush <piyushsaini766@gmail.com>
2026-07-15 21:55:38 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import urlRoutes from "./routes/url.js";
+
+import dns from "dns";
+dns.setServers([ // writeen because i am getting error...
+    '1.1.1.1',
+    '8.8.8.8'
+])
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+  })
+);
+app.use(express.json());
+
+app.use("/", urlRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
